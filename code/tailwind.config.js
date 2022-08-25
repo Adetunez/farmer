c4ecb8268983de8c13c65fcac2d3b348e91e49eb
Itunu <adeyemoitunu1@gmail.com>
2022-08-25 18:45:10 +0100

    project

---
+++ code/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'manmade1': '#416C00',
+      },
+    },
+  },
+  plugins: [],
+}
